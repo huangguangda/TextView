c869d36fbcb7760146f65ee9c1ea209b555123d5
huangguangda <2397923107@qq.com>
2017-10-02 17:22:36 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/textview/MyTextView.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.textview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTextView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_my_text_view );
+    }
+}
